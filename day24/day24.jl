830fbd30aa0cccde182081972a011d90d9a6e357
Robert <robert.cram@gmail.com>
2022-08-05 15:36:03 +0000

    Solves day 24.

--- day24/day24.jl
+++ day24/day24.jl
@@ -6,45 +6,56 @@ include("./../aoc.jl")
 
 using .AOC
 
-struct Sleigh
-    passenger::AbstractArray{Integer}
-    compartment1::AbstractArray{Integer}
-    compartment2::AbstractArray{Integer}
-end
-
 function AOC.processinput(data)
     data = parse.(Int64, split(data, '\n'))
 end
 
-function getgroup(groups, weights, availableweights, targetweight)
-    targetweight == 0 && (push!(groups, weights); return)
-    (targetweight < 0 || length(availableweights) == 0) && return
-    for (i, weight) in enumerate(availableweights)
-        getgroup(groups, push!(copy(weights), weight), availableweights[i+1:end], targetweight - weight)
+function getgroups(availableweights, targetweight, groups = [], weights = [])
+    targetweight == 0 && return push!(groups, weights)
+    (targetweight < 0 || length(availableweights) == 0) && return groups
+    (availableweights[begin] > targetweight) && return groups
+    for (i, weight) in enumerate(availableweights)        
+        groups = getgroups(availableweights[i+1:end], targetweight - weight, groups, push!(copy(weights), weight))
     end
-end
-
-function getgroups(avalaibleweights, targetweight)
-    groups = []
-    getgroup(groups, [], avalaibleweights, targetweight)
     groups
 end
 
+function solve(weights, groups)
+    weight = sum(weights) ÷ groups
+  
+    remainingweights = weights
+    groups1 = getgroups(remainingweights, weight)
+    groups1 = map(t -> t[3], sort(map(g-> (length(g), reduce(*, g), g), groups1)))
+
+    for group1 in groups1
+        remainingweights = setdiff(remainingweights, group1)
+        groups2 = getgroups(remainingweights, weight)
+        for group2 in groups2
+            remainingweights = setdiff(remainingweights, group2)
+            groups3 = getgroups(remainingweights, weight)  
+            groups == 3 && length(groups3) > 0 && return reduce(*, group1)
+            groups == 4 && for group3 in groups3
+                remainingweights = setdiff(remainingweights, group3)
+                groups4 = getgroups(remainingweights, weight)  
+                length(groups4) > 0 && return reduce(*, group1)
+            end
+        end
+    end
+end
 
 function solvepart1(weights)
-    weight = sum(weights) ÷ 3
-    getgroups(weights, weight)
+    solve(weights, 3)
 end
 
 function solvepart2(weights)
-    weights
+    solve(weights, 4)
 end
 
 puzzles = [
     Puzzle(24, "test 1", "input-test1.txt", solvepart1, 99),
-    # Puzzle(24, "deel 1", solvepart1, nothing),
-    # Puzzle(24, "test 2", "input-test1.txt", solvepart2, nothing),
-    # Puzzle(24, "deel 2", solvepart2, nothing)
+    Puzzle(24, "deel 1", solvepart1, 11266889531),
+    Puzzle(24, "test 2", "input-test1.txt", solvepart2, 44),
+    Puzzle(24, "deel 2", solvepart2, 77387711)
 ]
 
 printresults(puzzles)
